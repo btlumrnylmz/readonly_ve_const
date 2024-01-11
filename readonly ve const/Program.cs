67e1951da454a1816b15ac576b9d6ac26fe38605
btlumrnylmz <betulumran.6565@gmail.com>
2024-01-11 19:33:37 +0300

    Proje dosyası ekle.

---
+++ readonly ve const/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readonly_ve_const
+{
+    public class Araba
+    {
+        public readonly string Marka;
+        public const int tekerlekSayisi = 4;
+        public Araba(string marka)
+        {
+            Marka = marka;
+        }
+        public void Tanit()
+        {
+            Console.WriteLine($"Bu araba {Marka} marka ve {tekerlekSayisi} tekerlekli.");
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba = new Araba("Toyota");
+            araba.Tanit();
+
+        }
+    }
+}
